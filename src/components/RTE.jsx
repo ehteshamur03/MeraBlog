074fc162f21c7f11f7b70e137eb307fe93193bbe
Ehteshamur Rahman <ehteshamur03@gmail.com>
2025-01-04 17:48:08 +0530

    rte

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,14 +1,53 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import { Editor } from '@tinymce/tinymce-react';
-import { Controller } from 'react-hook-form';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { Controller } from "react-hook-form";
+import PropTypes from "prop-types";
 
-export default function RTE({ name, control, label, defaultValue = "" }) {
+export default function RTE({ name, control, label, defaultValue = "", height = 500 }) {
+  const editorConfig = {
+    initialValue: defaultValue,
+    icons: "thin",
+    readonly: false,
+    height,
+    menubar: true,
+    mobile: {
+      menubar: true,
+      plugins: "autosave lists autolink",
+      toolbar: "undo bold italic styles",
+    },
+    plugins: [
+      "image",
+      "advlist",
+      "autolink",
+      "lists",
+      "link",
+      "charmap",
+      "preview",
+      "anchor",
+      "searchreplace",
+      "visualblocks",
+      "fullscreen",
+      "insertdatetime",
+      "media",
+      "table",
+      "help",
+      "wordcount",
+    ],
+    toolbar:
+      "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+    content_style:
+      "body { font-family: Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6 }",
+    skin: "oxide",
+    content_css: "default",
+  };
+
   return (
     <div className="w-full space-y-4">
       {label && (
-        <label className="inline-block mb-2 pl-1 text-lg font-semibold text-gray-800">{label}</label>
+        <label className="inline-block mb-2 pl-1 text-lg font-semibold text-gray-800">
+          {label}
+        </label>
       )}
 
       <Controller
@@ -17,45 +56,16 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         render={({ field: { onChange } }) => (
           <Editor
             initialValue={defaultValue}
-            apiKey="kzysrma0k0q0e07zs3leab0ufufratduhvj7wq56w4cawq92"
-            init={{
-              initialValue: defaultValue,
-              readonly: false,
-              height: 500,
-              menubar: true,
-              plugins: [
-                "image",
-                "advlist",
-                "autolink",
-                "lists",
-                "link",
-                "image",
-                "charmap",
-                "preview",
-                "anchor",
-                "searchreplace",
-                "visualblocks",
-                "code",
-                "fullscreen",
-                "insertdatetime",
-                "media",
-                "table",
-                "code",
-                "help",
-                "wordcount",
-                "anchor",
-              ],
-              toolbar:
-                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-              content_style: "body { font-family: Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6 }",
-              skin: 'oxide',
-              content_css: 'default',
-            }}
+            apiKey={"kzysrma0k0q0e07zs3leab0ufufratduhvj7wq56w4cawq92"}
+            init={editorConfig}
             onEditorChange={onChange}
             className="rounded-lg border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         )}
       />
+
+      {/* Placeholder for validation messages */}
+      <p className="text-red-500 text-sm">{/* Validation message here */}</p>
     </div>
   );
 }
@@ -65,4 +75,12 @@ RTE.propTypes = {
   control: PropTypes.object.isRequired,
   label: PropTypes.string,
   defaultValue: PropTypes.string,
+  height: PropTypes.number,
+};
+
+RTE.defaultProps = {
+  name: "content",
+  defaultValue: "",
+  label: null,
+  height: 500,
 };
